bd2e6f1862111b84289de8e185e42581a20da9fe
mahesa60 <maheshasuro60@gmail.com>
2025-11-05 13:19:03 +0700

    Duplikasi sreen yang sudah dibuat sebelummnya

---
+++ lib/splash3.dart
@@ -0,0 +1,89 @@
+import 'package:flutter/material.dart';
+
+class MySplashScreen3 extends StatelessWidget {
+  const MySplashScreen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+            body: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    width: 250,
+                    height: 250,
+                    decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                      image: AssetImage("assets/images/D1.png")),
+                    color: Colors.deepPurple),
+                    ),
+                    SizedBox(height: 20),
+                    Text("Welcome",
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(height: 5),
+                    Text("Forgot to bring your wallet when you are shoping ?",
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                    SizedBox(height: 25),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 10,
+                          width: 10,
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple,  
+                            shape: BoxShape.circle),
+                        ),
+                        SizedBox(width: 10),
+                        Container(
+                          height: 10,
+                          width: 10,
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple[100]!,  
+                            shape: BoxShape.circle),
+                        ),
+                        SizedBox(width: 10),
+                        Container(
+                          height: 10,
+                          width: 10,
+                          decoration: BoxDecoration(
+                            color: Colors.deepPurple[100]!,  
+                            shape: BoxShape.circle),
+                        )
+                                  ],
+                                ),
+                                SizedBox(height: 20), 
+                                Container(
+                                  margin: EdgeInsets.only(left: 40, right: 40),
+                                  child: SizedBox(
+                                    width: double.infinity,
+                                    height: 40,
+                                    child: ElevatedButton(
+                                      onPressed: () {}, 
+                                  
+                                      style: ElevatedButton.styleFrom(backgroundColor: Colors.deepPurple),
+                                      child: Text("Selanjutnya",
+                                        style: TextStyle(
+                                        fontSize: 20.0,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                    ),
+                                  ),),
+                                  ),
+                                )
+                                    
+                  ],
+                ),
+            ),
+          );;
+  }
+}
