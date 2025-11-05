a96d020ec71ec784717323bbb409b6a016f21c8e
mahesa60 <maheshasuro60@gmail.com>
2025-11-05 14:48:26 +0700

    Membuat border email

--- lib/login.dart
+++ lib/login.dart
@@ -13,6 +13,45 @@ class Login extends StatelessWidget {
                   Icon(Icons.qr_code_2_outlined,
                   size: 80,
                   color: Colors.deepPurple),
+                  SizedBox(height: 20),
+                  Text("Welcome",
+                    style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 4),
+                  Text("Silahkan Login untuk melanjutkannya",
+                    style: TextStyle(
+                      fontSize: 16.0,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                  SizedBox(height: 20),
+                  Container(
+                    margin: EdgeInsets.only(left: 40, right: 40),
+                    child: TextField(
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: InputDecoration(
+                        labelText: "Email",
+                        hintText: "Masukan Email Anda",
+                        prefixIcon: Icon(Icons.email_outlined),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.deepPurple),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide(color: Colors.deepPurple,
+                          width: 2),
+                        ),
+                      ),
+                    ),
+                  ),
                 ],
               ),
             )
